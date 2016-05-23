d35b0d584992e309babf031fb8f3787aa8807531
keleifengdian <1031924372@qq.com>
2016-05-23 06:59:17 -0700

    ssh_crack

---
+++ pxssh__py.pi/pxsshh.py
@@ -0,0 +1,18 @@
+import pxssh
+
+def send_command(s,cmd):
+	s.sendline(cmd)
+	s.prompt()
+	print s.before
+
+def connect(host,user,password):
+	try:
+		s = pxssh.pxssh()
+		s.login(host,user,password)
+		return s
+	except:
+		print '[-] Error Connecting'
+		exit(0)
+
+s = connect('127.0.0.1','root','111111')
+send_command(s,'cat /etc/shadow | grep root')
